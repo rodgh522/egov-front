6398689bedd93013ac4cf23deb331834b5e1ccd1
gen oh <gen@gens-MacBook-Pro.local>
2026-01-28 14:51:01 +0900

    [Feat] update api

--- src/api/generated/apis/index.ts
+++ src/api/generated/apis/index.ts
@@ -1,9 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ActivityControllerApi';
 export * from './AuthenticationApi';
 export * from './BranchControllerApi';
+export * from './ContactControllerApi';
+export * from './CustomerControllerApi';
 export * from './GroupControllerApi';
+export * from './LeadControllerApi';
 export * from './MenuControllerApi';
+export * from './OpportunityControllerApi';
+export * from './PipelineStageControllerApi';
 export * from './PositionControllerApi';
+export * from './ProductControllerApi';
+export * from './QuoteControllerApi';
+export * from './SalesTargetControllerApi';
 export * from './TenantControllerApi';
 export * from './UserManagementApi';
